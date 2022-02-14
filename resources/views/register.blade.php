d486f04419321295d312c6501e3298583d2590c9
rickosong <ricko.aprilianto00@gmail.com>
2022-02-14 08:09:14 +0800

    menambahakan view register dan login

---
+++ resources/views/register.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="../css/bootstrap.min.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+		<link rel="stylesheet" href="../css/register.css" />
+		<title>Registrasi</title>
+	</head>
+	<body>
+		<!-- form registrasi -->
+		<section class="container">
+			<section class="row justify-content-md-start">
+				<section class="col-lg-5 col-md-4">
+					<div class="kanan">
+						<img src="../img/smkn2.png" alt="" />
+						<br /><br />
+						<h3>RESERVASI RUANGAN SMKN 2 BANJARMASIN</h3>
+					</div>
+				</section>
+			</section>
+
+			<section class="row justify-content-end">
+				<section class="col-sm-12 col-lg-5 col-md-12">
+					<form class="form-container">
+						<div class="form-group mb-3">
+							<h2>REGISTRASI</h2>
+							<br /><br />
+							<label for="username" class="teks-kolom">Username:</label>
+							<input type="email" class="form-control" id="username" aria-describedby="emailHelp" placeholder="Masukkan Username" />
+						</div>
+						<div class="form-group mb-3">
+							<label for="password" class="teks-kolom">Password:</label>
+							<input type="password" class="form-control" id="password" placeholder="Masukkan Password" />
+						</div>
+						<div class="form-group mb-3">
+							<label for="konfirmasipassword" class="teks-kolom">Konfirmasi Password:</label>
+							<input type="password" class="form-control" id="konfirmasipassword" placeholder="Konfirmasi Password" />
+						</div>
+						<div class="form-group mb-3">
+							<label for="password" class="teks-kolom">Nama:</label>
+							<input type="password" class="form-control" id="password" placeholder="Masukkan Nama Lengkap Anda" />
+						</div>
+						<div class="form-group mb-3">
+							<label for="password" class="teks-kolom">No.HP:</label>
+							<input type="password" class="form-control" id="password" placeholder="Masukkan Nomor HP Anda" />
+							<a href="loginpage.html"><small class="text-cl-sm">Sudah punya akun? Klik DIsini Untuk Login!</small></a
+							><br />
+							<a href="register.html"><small class="text-cl-sm">Forgot Password?</small></a>
+						</div>
+						<div class="d-grid gap-2">
+							<a href="loginpage.html" type="submit" class="tombol btn btn-block"
+								><span><i class="fa fa-user-plus" aria-hidden="true"></i></span> Daftar</a
+							>
+						</div>
+					</form>
+				</section>
+			</section>
+		</section>
+		<!-- form registrasi end -->
+	</body>
+
+	<!-- jQuery -->
+	<script src="../js/jquery-3.5.1.js"></script>
+	<script src="../js/jquery-3.6.0.min.js"></script>
+	<script src="../js/popper.min.js"></script>
+	<script src="../js/bootstrap.min.js"></script>
+	<!-- jQuery end -->
+</html>
