eb84c613cbea62adb4caecaebbc87a2203f20ec2
rickosong <ricko.aprilianto00@gmail.com>
2022-02-16 01:01:16 +0800

    menghubungkan file blade ke static file

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class RegisterController extends Controller
 {
     public function index(){
-        return view('login');
+        return view('auth.register');
     }
 }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,9 +4,9 @@
 		<meta charset="UTF-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<link rel="stylesheet" href="../css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-		<link rel="stylesheet" href="../css/login.css" />
+		<link rel="stylesheet" href="css/login.css" />
 		<title>Login</title>
 	</head>
 	<body>
@@ -51,9 +51,9 @@
 	</body>
 
 	<!-- jQuery -->
-	<script src="../js/jquery-3.5.1.js"></script>
-	<script src="../js/jquery-3.6.0.min.js"></script>
-	<script src="../js/popper.min.js"></script>
-	<script src="../js/bootstrap.min.js"></script>
+	<script src="js/jquery-3.5.1.js"></script>
+	<script src="js/jquery-3.6.0.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 	<!-- jQuery end -->
 </html>
