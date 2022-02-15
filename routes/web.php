f3f8fc04c7ac7cadc22c9898573bbcb19dacdf53
rickosong <ricko.aprilianto00@gmail.com>
2022-02-16 00:58:12 +0800

    adding static file

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\Auth\RegisterController;
 
 /*
 |--------------------------------------------------------------------------
