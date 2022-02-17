3a4b48c76a5eaf32f0f1141fc03de1d75de3ccdf
rickosong <ricko.aprilianto00@gmail.com>
2022-02-17 17:41:06 +0800

    edit middleware in routes

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,9 @@ use App\Http\Controllers\User\HomeController;
 */
 
 // Auth
-Route::get('/register', [RegisterController::class, 'index'])->name('register');
+Route::get('/register', [RegisterController::class, 'index'])->name('register')->middleware('guest');
 Route::post('/register', [RegisterController::class, 'regisMember']);
-Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'loginMember']);
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
 
