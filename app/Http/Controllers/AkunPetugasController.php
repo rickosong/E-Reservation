ef76c63cfe5cd326beb0360685295c0f1ce5b615
rickosong <ricko.aprilianto00@gmail.com>
2022-03-06 01:51:43 +0800

    make all of the method in akunpetugascontroller

--- app/Http/Controllers/AkunPetugasController.php
+++ app/Http/Controllers/AkunPetugasController.php
@@ -9,4 +9,21 @@ class AkunPetugasController extends Controller
     public function index(){
         return view('akun-petugas');
     }
+
+    public function add(){
+        return view('akun-petugas');
+    }
+
+    public function edit(){
+        return view('akun-petugas');
+    }
+
+    public function update(){
+        return view('akun-petugas');
+    }
+
+    public function destroy(){
+        return view('akun-petugas');
+    }
+
 }
