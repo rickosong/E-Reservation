6cf8db5629062141ad0ecdc28c7fe7bb9f6ed390
rickosong <ricko.aprilianto00@gmail.com>
2022-03-30 23:24:26 +0800

    sudah bisa melakukan update otomatis bila pesanan tidak dibayar melebihi waktu checkin

--- app/Http/Controllers/PesananController.php
+++ app/Http/Controllers/PesananController.php
@@ -12,11 +12,11 @@ class PesananController extends Controller
 {
     public function index(){
 
-        // Penyewaan::where('checkout' <= date('Y'))
         // $date = Carbon::now()
         date_default_timezone_set("Asia/Makassar");
-        $date = date('Y/m/d H/i/s');
-        dd($date);
+        $date = date('Y/m/d H/i');
+        Penyewaan::where('checkout', '<=', date('Y-m-d H:i'))->where('status_id', 3)->update(['status_id' => 2]);
+        // dd($date);
 
         return view('pesanan', [
             "title" => "Dashboard Admin | Pesanan",
@@ -36,12 +36,25 @@ class PesananController extends Controller
     }
 
     public function store(Request $request){
+        date_default_timezone_set("Asia/Makassar");
+        $date = date('Y/m/d H/i');
+
+        $str        = "";
+        $characters = '123456789';
+        $max        = strlen($characters) - 1;
+        for ($i = 0; $i < 8; $i++) {
+            $rand = mt_rand(0, $max);
+            $str .= $characters[$rand];
+        }
+
         $penyewaan = New Penyewaan;
 
         // dd($request->jamawal);
 
         $penyewaan->ruangan_id = $request->idruangan;
         $penyewaan->user_id = auth()->user()->id;
+        $penyewaan->nomorpemesan = auth()->user()->id . $str . date('d');
+        $penyewaan->tanggalpemesanan = $date;
         $penyewaan->checkin = $request->jamawal;
         $penyewaan->checkout = $request->jamakhir;
         $penyewaan->status_id = 3;
